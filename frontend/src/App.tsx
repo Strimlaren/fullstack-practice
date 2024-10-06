1db97ed16b3c8934ae8e3a6c1ea9ceb163af5d15
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2024-10-06 21:59:40 +0200

    started modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@ import Header from "./components/Header";
 
 import { useState } from "react";
 
+import LoginModal from "./components/LoginModal";
+
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
   return (
     <div className="flex flex-col">
+      <LoginModal />
       <Header isLoggedIn={isLoggedIn} />
       <div className="p-4">
         <p className="text-3xl">APP</p>
--- frontend/src/components/LoginModal.tsx
+++ frontend/src/components/LoginModal.tsx
@@ -1,5 +1,9 @@
 const LoginModal = () => {
-  return <div>LoginModal</div>;
+  return (
+    <div className="absolute flex justify-center items-center bg-black bg-opacity-60 w-screen h-screen">
+      <h2 className="text-5x1">Welcome to SPAM Email</h2>
+    </div>
+  );
 };
 
 export default LoginModal;
